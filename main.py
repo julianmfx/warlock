19a86faa5ef9635cdaa82a5366f5a6d55b48abe7
julianmfx <julianmfx@gmail.com>
2026-05-05 17:20:16 -0600

    feat(main): add data_engineer mock Agent instance in main.py for local testing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from warlock!")
+# main.py
 
+# imports
+from warlock.agent import Agent
+from warlock.memory import Memory
 
-if __name__ == "__main__":
-    main()
+m = Memory()
+m.write("problem_statement", "Ingest CSV into BigQuery")
+
+data_engineer = Agent(
+    name="data_engineer",
+    identity="I am a data engineer with over 30 years of experience. I move data, create data pipelines, build data models. I do pipelines, ingestion, transformation, schemas and data quality checks",
+    memory=m,
+)
+
+print(data_engineer.describe())
